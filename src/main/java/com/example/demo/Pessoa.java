7fcb2fb234a83781bc55b669fcd05ee9ed18a86d
Adarlan Teixeira <adarlan18@gmail.com>
2020-04-17 17:42:41 -0300

    Add constructors

--- src/main/java/com/example/demo/Pessoa.java
+++ src/main/java/com/example/demo/Pessoa.java
@@ -4,12 +4,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Pessoa {
 
     @Id
